4304c6f2a36e7396bef71f5c5b1dd9a3a22d698a
MelonFruit7 <leogofman23@gmail.com>
2025-05-08 20:43:25 -0400

    Texture Context

---
+++ frontend/src/app/components/texture.tsx
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import * as THREE from "three";
+
+const useTextureStore = create((set) => ({
+  texture: new THREE.DataTexture(new Uint8Array([0, 0, 0, 0]), 1, 1, THREE.RGBAFormat),
+  base64: "",
+  setTexture: (base64_: string) => {
+    set({ base64: base64_ })
+
+    const texture = new THREE.TextureLoader().load(
+      base64_,
+      () => {
+        set({ texture });
+      },
+      undefined,
+      (err) => {
+        console.error("Texture loading failed:", err);
+      }
+    );
+  },
+}));
+
+export default useTextureStore;
